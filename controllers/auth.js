41c6684a005a4627183305536ba84b3ed8c1e20e
omar.barrios <omar.barrios@alumnos.uneatlantico.es>
2021-08-31 00:46:25 +0200

    [fix] autenticacion con google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response ) => {
 
@@ -50,7 +51,55 @@ const login = async (req, res = response ) => {
     }
 }
 
+const googleSignin = async (req, res = response) => {
+    
+    const {id_token} = req.body;
+
+    try {
+
+        const { correo, nombre, img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if (!usuario) {
+            //tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            };
+
+            usuario= new Usuario(data);
+            await usuario.save();
+        }
+
+        //si el usuario en db
+        if (!usuario.estado) {
+            res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado',
+            })
+        }
+
+        // generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            msg: 'Tooken de google no es valido'
+        })
+    }
+}
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
